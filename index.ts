8309d129b30f923f4739b0334322ead71c68ef09
Sabrina-barreiros <barreiros890@gmail.com>
2022-02-18 22:14:16 -0300

    1 file modified

--- index.ts
+++ index.ts
@@ -19,9 +19,14 @@ let mySentence: string='bla blaa';
 //Utilizando o console
 console.log("Olá, mundo!");
 
+//DEclarando arrays
+let list: Number[] = [21, 2, 65, 55, 10, 98, 3, 9, 80, 42];
+
 //Utilizando laços de repetição
-for(let i=1; i==10; i++){
+console.log("Laço");
+for(let i=1; i<10; i++){
   console.log(i);
+  console.log(list[i]);
 }
 
 //Utilizando condicionais
@@ -31,8 +36,28 @@ if(myVar1 %2 == 0)
 }else{
   console.log(myVar1+" é Impar");
 }
+
+
+
 //Utilizando laços e condicionais
+for(let i=1; i<=10; i++){
+  if (i%2 == 0){
+    console.log(i);
+  }
+}
+let counter = 0;
+let intervalId = setInterval(() =>{
+  counter += 1;
+  const counterDiv: HTMLElement= document.getElementById('counterDiv');
+  counterDiv.innerHTML = 'Contador:' +counter;
+}
+,1000);
+
+let title: string = 'Olá, mundo!';
+let paragraph: string = 'Bla bla';
 
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+appDiv.innerHTML = `<h1>${title}</h1>
+<p>${paragraph}</p>
+<div id='counterDiv'>}</div>`;
