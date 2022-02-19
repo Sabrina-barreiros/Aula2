dd22641b16495d85a586f728a357b5dabbde5ed0
Sabrina-barreiros <barreiros890@gmail.com>
2022-02-18 21:49:47 -0300

    1 file modified

--- index.ts
+++ index.ts
@@ -1,6 +1,38 @@
 // Import stylesheets
 import './style.css';
 
+/*Comentando
+ em varias
+ linhas*/
+
+//Declaração de constantes
+const myVar1: Number = 42;
+const myVar2: Number = 42;
+
+//Declaração de variaveis
+let myVar3: 42;
+let myVar4: Number = 42;
+let myVar5: string ='Olá';
+
+let mySentence: string='bla blaa';
+
+//Utilizando o console
+console.log("Olá, mundo!");
+
+//Utilizando laços de repetição
+for(let i=1; i==10; i++){
+  console.log(i);
+}
+
+//Utilizando condicionais
+if(myVar1 %2 == 0)
+{
+  console.log(myVar1+" é Par");
+}else{
+  console.log(myVar1+" é Impar");
+}
+//Utilizando laços e condicionais
+
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
